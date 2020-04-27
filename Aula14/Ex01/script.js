9d9440f2e856a97b67524b4886f612b7d50e8e38
lishibashy <luciano.ishibashy@gmail.com>
2020-04-27 13:56:23 -0300

    Exercicios usando laços de repetição

---
+++ Aula14/Ex01/script.js
@@ -0,0 +1,11 @@
+function contar(){
+    var inicio = window.document.getElementById('inicio');
+    var fim = window.document.getElementById('final');
+    var passo = window.document.getElementById('passo');
+    a = 1
+    b = 8
+    c = 1
+    for (a; a <= b; a = a + c){
+        res.innerHTML = `Contando: ${a}`;
+    }
+}
---
+++ Aula14/exemplo-for-01.js
@@ -0,0 +1,14 @@
+console.log('vai começar...');
+for(var c = 1; c <= 4; c++){
+    console.log(c);
+}
+console.log('Fim!');
+
+var inicio = 1;
+var final = 10;
+var passo = 2;
+console.log('vai começar 2...');
+for(inicio; inicio <= final; inicio = inicio + passo){
+    console.log(inicio);
+}
+console.log('Fim!');
